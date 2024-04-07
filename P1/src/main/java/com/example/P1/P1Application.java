9b20bb5cfd078b59b6b14627ea3609237a7ac6fb
vladvadean <vadean.vlad23@gmail.com>
2024-04-07 15:33:15 +0300

    last lab work only

---
+++ P1/src/main/java/com/example/P1/P1Application.java
@@ -0,0 +1,19 @@
+package com.example.P1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * main class that contains the main method and
+ * runs the whole project
+ */
+@SpringBootApplication
+public class P1Application {
+
+    public static void main(String[] args) {
+        //String cypher = CesarCypher.GenerateCypher("ANA",CypherDifficulty.SIMPLE);
+        // System.out.println(cypher);
+        SpringApplication.run(P1Application.class, args);
+    }
+
+}
