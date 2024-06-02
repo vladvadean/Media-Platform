4b39f4a760667b60af96681e551dc1a91e7a3c13
vladvadean <vadean.vlad23@gmail.com>
2024-06-02 21:41:54 +0300

    front end updated

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {HomeComponent} from "./components/home/home.component";
 import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, RouterLink, RouterLinkActive, NgOptimizedImage],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import {Routes} from '@angular/router';
-import {HomeComponent} from "./components/home/home.component";
 import {AccountComponent} from "./components/account/account.component";
 import {SignInSignUpComponent} from "./components/sign-in-sign-up/sign-in-sign-up.component";
 import {AdminAccountComponent} from "./components/admin-account/admin-account.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: 'signInSignUp', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
   {path: 'account', component: AccountComponent},
   {path: 'signInSignUp', component: SignInSignUpComponent},
   {path: 'adminAccount', component: AdminAccountComponent}
